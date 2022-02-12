eb138736558dd50b33f9b644c23c28b2c490f020
rokibulslam <rokibulslam6@gmail.com>
2022-02-12 19:45:47 +0600

    server setup done

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+
+const ObjectId = require("mongodb").ObjectId;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connecting MongoDB database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2efaz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+    try {
+        
+        
+    } finally {
+        // await client.close()
+    }
+}
+
+
+run().catch(console.dir);
+
+// Checking server
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`listening at port ${port}`);
+});
